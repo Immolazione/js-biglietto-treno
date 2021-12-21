cb50b79bfb99e31526110cf975bc3492dc14ccbc
Immolazione <federico.deered@gmail.com>
2021-12-21 16:10:56 +0100

    add validator for kms and age

--- js/script.js
+++ js/script.js
@@ -11,15 +11,20 @@ va applicato uno sconto del 40% per gli over 65.
 /*
 1- Chiedere numero chilometri
 2- Chiedere età passeggero
+2b- Valutazione valori
 3- Calcolare il prezzo del viaggio con gli sconti
 4- Print
 */
 
 // 1-2
 const userAge = parseInt(prompt('Quanti anni hai?', '24').trim());
-console.log(userAge);
+console.log(`userAge: ${userAge}`);
 const userKm = parseInt(prompt('Quanti chilometri vuoi percorrere?', '100').trim());
-console.log(userKm);
+console.log(`userKm: ${userKm}`);
+
+if (isNaN(userAge) || isNaN(userKm)) {
+    alert('I VALORI INSERITI SONO INVALIDI')
+}
 
 // 3
 let ticketPrice = 0.21 * userKm;
@@ -33,13 +38,13 @@ if (userAge < 18) {
 }
 
 //4
-console.log(ticketPrice);
+console.log(`ticketPrice: ${ticketPrice}`);
 
 const displayAge = document.getElementById('age');
-displayAge.innerHTML = `${userAge} €`;
+displayAge.innerHTML = `Età Passeggero: ${userAge} anni`;
 
 const displayKm = document.getElementById('kms');
-displayKm.innerHTML = `${userKm} Km`;
+displayKm.innerHTML = `Km Percorsi: ${userKm} Km`;
 
 const displayPrice = document.getElementById('price');
-displayPrice.innerHTML = `${ticketPrice} €`
+displayPrice.innerHTML = `Prezzo Totale: ${ticketPrice} €`
