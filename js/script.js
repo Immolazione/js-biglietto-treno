4eeb20d34500966005f479cf776f15e45f81ed2d
Immolazione <federico.deered@gmail.com>
2021-12-21 14:10:37 +0100

    add points to follow

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 */
+
+/*
+1- Chiedere numero chilometri
+2- Chiedere età passeggero
+3- Calcolare il prezzo del viaggio con gli sconti
+4- Print
+*/
