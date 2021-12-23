453726c66390cc41d43a40d8e855736c3c290c63
Immolazione <federico.deered@gmail.com>
2021-12-23 15:00:37 +0100

    fix ;s and add else

--- js/script.js
+++ js/script.js
@@ -21,30 +21,31 @@ const userAge = parseInt(prompt('Quanti anni hai?', '24').trim());
 console.log(`userAge: ${userAge}`);
 const userKm = parseInt(prompt('Quanti chilometri vuoi percorrere?', '100').trim());
 console.log(`userKm: ${userKm}`);
-
-if (isNaN(userAge) || isNaN(userKm)) {
-    alert('I VALORI INSERITI SONO INVALIDI')
-}
-
+  
 // 3
 let ticketPrice = 0.21 * userKm;
 const youngDiscount = (ticketPrice * 20) / 100;
 const elderDiscount = (ticketPrice * 40) / 100;
 
-if (userAge < 18) {
-    ticketPrice = ticketPrice - youngDiscount;
-} else if (userAge > 65) {
-    ticketPrice = ticketPrice - elderDiscount;
+if (isNaN(userAge) || isNaN(userKm)) {
+    alert('I VALORI INSERITI SONO INVALIDI');
+} else {
+    
+    if (userAge < 18) {
+        ticketPrice = ticketPrice - youngDiscount;
+    } else if (userAge > 65) {
+        ticketPrice = ticketPrice - elderDiscount;
+    }
 }
-
-//4
-console.log(`ticketPrice: ${ticketPrice.toFixed(2)} €`);
-
-const displayAge = document.getElementById('age');
-displayAge.innerHTML = `Età Passeggero: ${userAge} anni`;
-
-const displayKm = document.getElementById('kms');
-displayKm.innerHTML = `Km Percorsi: ${userKm} Km`;
-
-const displayPrice = document.getElementById('price');
-displayPrice.innerHTML = `Prezzo Totale: ${ticketPrice.toFixed(2)} €`
+    
+    //4
+    console.log(`ticketPrice: ${ticketPrice.toFixed(2)} €`);
+    
+    const displayAge = document.getElementById('age');
+    displayAge.innerHTML = `Età Passeggero: ${userAge} anni`;
+    
+    const displayKm = document.getElementById('kms');
+    displayKm.innerHTML = `Km Percorsi: ${userKm} Km`;
+    
+    const displayPrice = document.getElementById('price');
+    displayPrice.innerHTML = `Prezzo Totale: ${ticketPrice.toFixed(2)} €`;
